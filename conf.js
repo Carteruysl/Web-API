0df7c3ce00899f10b31014d77e899fe30b6c1874
Carteruysl <wcarter9527@gmail.com>
2019-08-24 14:44:18 +0800

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,12 @@
+module.exports = {
+    db: {
+        host:'127.0.0.1',
+        user:'root',
+        password:'123456',
+        database:'customer',
+        port:3306
+    },
+    port: 3000
+    // 自訂密碼的加鹽
+    //salt: '@2#!A9x?3'
+};
